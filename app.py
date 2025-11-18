b5a8b7b40874b69a6b0f2dc84fc7a143100f87d1
Ryotaa12 <jade.banagan@dorsu.edu.ph>
2025-11-18 17:09:22 +0800

    Deploy Paas

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return 'Hello, Jade! This is a PaaS Deployment.'
+ return '2+2'
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=8080)
