7f32c94f858a0bcc42d310e4dbe97ba63c761106
Jade Banagan <128209139+Ryotaa12@users.noreply.github.com>
2025-11-18 17:51:20 +0800

    Modify product function to print result
    Changed the product function to print the product instead of returning it.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def product():
  num1= 2
  num2 = 2
  product = multiply (num1, num2)
- return product
+ print ( product )
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=8080)
