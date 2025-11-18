eaca42d893f121bf390b0766a1e296183a0fb1fd
Jade Banagan <128209139+Ryotaa12@users.noreply.github.com>
2025-11-18 17:53:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
+def product()
  num1= 2
  num2 = 2
  product = multiply (num1, num2)
- print ( product )
+ print f "<H1>{product}</H1>"
+ return product
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=8080)
